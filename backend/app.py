77eafa2454b2b93d46c3876a3ad162e354ed079a
namansharma28 <namansharma.web@gmail.com>
2025-05-30 22:14:10 +0530

    added cors url

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import json
 import os
 
 app = Flask(__name__, static_folder="../frontend", static_url_path="/")
-CORS(app)
+CORS(app, origins=["https://emptycup-frontend-smoky.vercel.app"])
 
 # Serve the index.html
 @app.route("/")
